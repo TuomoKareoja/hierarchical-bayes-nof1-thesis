87da2ded905b1ec825bb5d3a26bf9366b06271c7
Tuomo Kareoja <tuomo.kareoja@gmail.com>
2020-04-21 19:18:13 +0200

    reformat and change r to python

--- makefile
+++ makefile
@@ -1,37 +1,115 @@
-############################################################################
-# If you don't deploy locally remember to change the username and password #
-############################################################################
+#
+.PHONY: clean data lint requirements sync_data_to_s3 sync_data_from_s3
 
-username = rstudio
-password = password
-container_name = r-bayes-env
-image_name = r-bayes-env
-rstudio_settings_path = user-settings
+#################################################################################
+# GLOBALS                                                                       #
+#################################################################################
 
-# Builds and starts the container with above parameters using specified rstudio settings
-run:
-	docker build -t $(image_name) .
-	docker run --name $(container_name) \
-	-d -p 8787:8787 \
-	-v "`pwd`":/home/rstudio/working \
-	-e USERNAME=$(username) -e PASSWORD=$(password) -e ROOT=TRUE \
-	-d $(image_name)
-	# use provided settings. If no settings provided uses defaults
-	docker cp $(rstudio_settings_path) $(container_name):/home/rstudio/.rstudio/monitored/user-settings/
+PROJECT_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+PROJECT_NAME = bayes_nof1
 
-# stops and removes the container
-stop:
-	docker stop $(container_name)
-	docker rm $(container_name)
+ifeq (,$(shell which conda))
+HAS_CONDA=False
+else
+HAS_CONDA=True
+endif
 
-# prints docker container log
-debug:
-	docker logs $(container_name)
+#################################################################################
+# COMMANDS                                                                      #
+#################################################################################
 
-# starts bash in container
-shell:
-	docker exec -t -i $(container_name) bash
+## Make Dataset
+data:
+	python src/data/make_dataset.py
 
-# copy rstudio settings from container to host (overwrites previous settings!)
-settings:
-	docker cp $(container_name):/home/rstudio/.rstudio/monitored/user-settings/user-settings $(rstudio_settings_path)
+## Delete all compiled Python files
+clean:
+	find . -type f -name "*.py[co]" -delete
+	find . -type d -name "__pycache__" -delete
+
+## Lint using flake8
+lint:
+	flake8 src
+
+## Set up python interpreter environment
+environment:
+ifeq (True,$(HAS_CONDA))
+	@echo ">>> Detected conda, creating conda environment."
+	conda create --no-default-packages --yes --name $(PROJECT_NAME) python=3
+	@echo ">>> New conda env created. Activate with:\nsource activate $(PROJECT_NAME)"
+else
+	@echo ">>> Enviroment could not be created. Install conda first"
+endif
+
+## Install Python Dependencies
+requirements:
+ifeq (True,$(HAS_CONDA))
+	@echo ">>> Detected conda, updating activated conda environment."
+	conda env update --file environment.yml
+	# adding src-files to enviroment for easy importing
+	python -m pip install -e .
+	@echo ">>> Conda enviroment updated"
+else
+	@echo ">>> Environment could not be updated. Install conda first"
+endif
+
+#################################################################################
+# Self Documenting Commands                                                     #
+#################################################################################
+
+.DEFAULT_GOAL := help
+
+# Inspired by <http://marmelab.com/blog/2016/02/29/auto-documented-makefile.html>
+# sed script explained:
+# /^##/:
+# 	* save line in hold space
+# 	* purge line
+# 	* Loop:
+# 		* append newline + line to hold space
+# 		* go to next line
+# 		* if line starts with doc comment, strip comment character off and loop
+# 	* remove target prerequisites
+# 	* append hold space (+ newline) to line
+# 	* replace newline plus comments by `---`
+# 	* print line
+# Separate expressions are necessary because labels cannot be delimited by
+# semicolon; see <http://stackoverflow.com/a/11799865/1968>
+.PHONY: help
+help:
+	@echo "$$(tput bold)Available rules:$$(tput sgr0)"
+	@echo
+	@sed -n -e "/^## / { \
+		h; \
+		s/.*//; \
+		:doc" \
+		-e "H; \
+		n; \
+		s/^## //; \
+		t doc" \
+		-e "s/:.*//; \
+		G; \
+		s/\\n## /---/; \
+		s/\\n/ /g; \
+		p; \
+	}" ${MAKEFILE_LIST} \
+	| LC_ALL='C' sort --ignore-case \
+	| awk -F '---' \
+		-v ncol=$$(tput cols) \
+		-v indent=19 \
+		-v col_on="$$(tput setaf 6)" \
+		-v col_off="$$(tput sgr0)" \
+	'{ \
+		printf "%s%*s%s ", col_on, -indent, $$1, col_off; \
+		n = split($$2, words, " "); \
+		line_length = ncol - indent; \
+		for (i = 1; i <= n; i++) { \
+			line_length -= length(words[i]) + 1; \
+			if (line_length <= 0) { \
+				line_length = ncol - indent - length(words[i]) - 1; \
+				printf "\n%*s ", -indent, " "; \
+			} \
+			printf "%s ", words[i]; \
+		} \
+		printf "\n"; \
+	}' \
+	| more $(shell test $(shell uname) = Darwin && echo '--no-init --raw-control-chars')
---
+++ src/data/simulating_data.py
@@ -0,0 +1,191 @@
+# %%
+
+import pandas as pd
+import numpy as np
+import random
+import os
+import statsmodels.api as sm
+
+# %%
+
+seed = 123
+
+np.random.seed(123)
+random.seed(123)
+
+# %%
+
+data_folder = os.path.join("..", "..", "data")
+params_file_name = "patient_parameters.csv"
+measurements_file_name = "patient_measurements.csv"
+params_path = os.path.join(data_folder, params_file_name)
+measurements_path = os.path.join(data_folder, measurements_file_name)
+
+# %%
+
+# Study design
+patients_n = 40
+# must be an even number, because of balanced design
+blocks_n = 4
+treatment_measurements_n = 7
+# treatment and no treatment only. No multiple treatments
+total_measurements_n = blocks_n * treatment_measurements_n * 2
+
+# %%
+
+# Population Level Parameters
+
+# normal distribution
+population_base_level_mean = 10
+population_base_level_sd = 1
+population_treatment_effect_mean = 1.5
+population_treatment_effect_sd = 0.9
+population_trend_mean = 0.02
+population_trend_sd = 0.01
+# gamma distribution
+population_measurement_error_shape = 0.02
+population_measurement_error_scale = 0.005
+# beta distribution
+population_autocorrelation_alpha = 80
+population_autocorrelation_beta = 200
+
+
+# Patient Level Parameters
+patient_base_level_array = np.random.normal(
+    population_base_level_mean, population_base_level_sd, patients_n
+)
+patient_treatment_effect_array = np.random.normal(
+    population_treatment_effect_mean, population_treatment_effect_sd, patients_n
+)
+patient_trend_list = np.random.normal(
+    population_trend_mean, population_trend_sd, patients_n
+)
+patient_measurement_error_sd = np.random.gamma(
+    population_measurement_error_shape, population_measurement_error_scale, patients_n
+)
+patient_autocorrelation = np.random.beta(
+    population_autocorrelation_alpha, population_autocorrelation_beta, patients_n
+)
+
+# %%
+
+# creating patient dataframe
+patient_params_df = pd.DataFrame(
+    {
+        "patient_number": [i for i in range(1, patients_n + 1)],
+        "base_level": patient_base_level_array,
+        "trend": patient_treatment_effect_array,
+        "treatment_effect": patient_treatment_effect_array,
+        "measurement_error_sd": patient_treatment_effect_array,
+        "autocorrelation": patient_autocorrelation,
+    }
+)
+
+# %%
+
+# Randomizing patient treatment order
+
+
+patient_treatment_orders_list = []
+
+for patient in range(patients_n):
+
+    treatment_order_list = []
+
+    for i in range(blocks_n // 2):
+        block = [0, 0]
+        # randomizing the treatment to first or second slot in the block
+        # 1 marks the effective treatment and 0 is the default
+        effective_treatment_index = np.random.binomial(1, 0.5)
+        block[effective_treatment_index] = 1
+
+        treatment_order_list.extend(block)
+        # add the balancing block
+        treatment_order_list.extend(block[::-1])
+
+    patient_treatment_orders_list.append(treatment_order_list)
+
+
+patient_params_df["treatment_order"] = patient_treatment_orders_list
+
+# %%
+
+patient_measurements_df_list = []
+
+for index, patient in patient_params_df.iterrows():
+
+    # AUTOCORRELATED ERRORS
+    # no autoregressive component
+    ar = np.array([1, 0])
+    # lag 1 moving average component
+    ma = np.array([1, patient["autocorrelation"]])
+    arma_process = sm.tsa.ArmaProcess(ar, ma)
+    ma_process_array = arma_process.generate_sample(
+        nsample=total_measurements_n, scale=patient["measurement_error_sd"],
+    )
+
+    # BASE LEVEL AND TREND
+    base_and_trend_array = np.array(
+        [
+            patient["base_level"] + patient["trend"] * measurement_index
+            for measurement_index in range(total_measurements_n)
+        ]
+    )
+
+    # TREATMENT EFFECT
+    treatment_effects_array = np.array(
+        list(
+            pd.core.common.flatten(
+                [
+                    [patient["treatment_effect"] * indicator] * treatment_measurements_n
+                    for indicator in patient["treatment_order"]
+                ]
+            )
+        )
+    )
+
+    measurements = ma_process_array + base_and_trend_array + treatment_effects_array
+
+    patient_df = pd.DataFrame(
+        {
+            "patient_number": [patient["patient_number"]] * total_measurements_n,
+            "measurement_index": [
+                measurement_index + 1
+                for measurement_index in range(total_measurements_n)
+            ],
+            "treatment_period_index": list(
+                pd.core.common.flatten(
+                    [
+                        [treatment_period_index + 1] * treatment_measurements_n
+                        for treatment_period_index in range(blocks_n * 2)
+                    ]
+                )
+            ),
+            "block_index": list(
+                pd.core.common.flatten(
+                    [
+                        [block_index + 1] * treatment_measurements_n * 2
+                        for block_index in range(blocks_n)
+                    ]
+                )
+            ),
+            "treatment": list(
+                pd.core.common.flatten(
+                    [
+                        [indicator] * treatment_measurements_n
+                        for indicator in patient["treatment_order"]
+                    ]
+                )
+            ),
+            "measurement": measurements,
+        }
+    )
+
+    patient_measurements_df_list.append(patient_df)
+
+patient_measurements_df = pd.concat(patient_measurements_df_list, ignore_index=True)
+
+# %%
+
+patient_params_df.to_csv(params_path, index=False)
+patient_measurements_df.to_csv(measurements_path, index=False)
