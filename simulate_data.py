e46a5a1a80592b64584fbfd3d6bebd90dd1250e0
Tuomo Kareoja <tuomo.kareoja@gmail.com>
2020-05-17 16:24:19 +0200

    data simulation clean up

--- simulate_data.py
+++ simulate_data.py
@@ -1,21 +1,50 @@
 # %%
 
-import pandas as pd
-import numpy as np
-import scipy
-import matplotlib.pyplot as plt
-import random
 import os
-import statsmodels.api as sm
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy
+from dotenv import load_dotenv
+
+from src.create_patient_measurements import create_patient_measurements
+
+load_dotenv()
 
 plt.style.use("seaborn-white")
 
 # %%
 
-seed = 123
+seed = int(os.getenv("SEED"))
 
-np.random.seed(123)
-random.seed(123)
+np.random.seed(seed)
+random.seed(seed)
+
+patients_n = int(os.getenv("PATIENTS_N"))
+blocks_n = int(os.getenv("BLOCKS_N"))
+treatment_measurements_n = int(os.getenv("TREATMENT_MEASUREMENTS_N"))
+
+# treatment and no treatment only. No multiple treatments
+total_measurements_n = blocks_n * treatment_measurements_n * 2
+
+population_treatment1_mean = float(os.getenv("POPULATION_TREATMENT1_MEAN"))
+population_treatment1_sd = float(os.getenv("POPULATION_TREATMENT1_SD"))
+population_treatment2_mean = float(os.getenv("POPULATION_TREATMENT2_MEAN"))
+population_treatment2_sd = float(os.getenv("POPULATION_TREATMENT2_SD"))
+population_trend_mean = float(os.getenv("POPULATION_TREND_MEAN"))
+population_trend_sd = float(os.getenv("POPULATION_TREND_SD"))
+
+population_measurement_error_shape = float(
+    os.getenv("POPULATION_MEASUREMENT_ERROR_SHAPE")
+)
+population_measurement_error_scale = float(
+    os.getenv("POPULATION_MEASUREMENT_ERROR_SCALE")
+)
+
+population_autocorrelation_alpha = float(os.getenv("POPULATION_AUTOCORRELATION_ALPHA"))
+population_autocorrelation_beta = float(os.getenv("POPULATION_AUTOCORRELATION_BETA"))
 
 # %%
 
@@ -26,36 +55,8 @@ measurements_file_name = "patient_measurements.csv"
 params_path = os.path.join(data_folder, params_file_name)
 measurements_path = os.path.join(data_folder, measurements_file_name)
 
-# %%
+# %% Creating patient level parameters
 
-# Study design
-patients_n = 10
-# must be an even number, because of balanced design
-blocks_n = 4
-treatment_measurements_n = 4
-# treatment and no treatment only. No multiple treatments
-total_measurements_n = blocks_n * treatment_measurements_n * 2
-
-# %%
-
-# Population Level Parameters
-
-# normal distribution
-population_treatment1_mean = 10
-population_treatment1_sd = 0.2
-population_treatment2_mean = 9.7
-population_treatment2_sd = 0.3
-population_trend_mean = 0.02
-population_trend_sd = 0.01
-# inverse gamma distribution
-population_measurement_error_shape = 3
-population_measurement_error_scale = 0.3
-# beta distribution
-population_autocorrelation_alpha = 100
-population_autocorrelation_beta = 200
-
-
-# Patient Level Parameters
 patient_treatment1_array = np.random.normal(
     population_treatment1_mean, population_treatment1_sd, patients_n
 )
@@ -65,7 +66,7 @@ patient_treatment2_array = np.random.normal(
 patient_trend_array = np.random.normal(
     population_trend_mean, population_trend_sd, patients_n
 )
-# scipy uses the numpy random seed
+# scipy shares the numpy random seed
 patient_measurement_error_sd_array = scipy.stats.invgamma.rvs(
     a=population_measurement_error_shape,
     scale=population_measurement_error_scale,
@@ -184,7 +185,6 @@ patient_params_df = pd.DataFrame(
 
 # Randomizing patient treatment order
 
-
 patient_treatment_orders_list = []
 
 for patient in range(patients_n):
@@ -213,49 +213,16 @@ patient_measurements_df_list = []
 
 for index, patient in patient_params_df.iterrows():
 
-    # AUTOCORRELATED ERRORS
-    # no autoregressive component
-    ar = np.array([1, 0])
-    # lag 1 moving average component
-    ma = np.array([1, patient["autocorrelation"]])
-    arma_process = sm.tsa.ArmaProcess(ar, ma)
-    ma_process_array = arma_process.generate_sample(
-        nsample=total_measurements_n, scale=patient["measurement_error_sd"],
+    measurements = create_patient_measurements(
+        total_measurements_n=total_measurements_n,
+        treatment_measurements_n=treatment_measurements_n,
+        treatment_indicator_list=patient["treatment_order"],
+        treatment1=patient["treatment1"],
+        treatment2=patient["treatment2"],
+        trend=patient["trend"],
+        autocorrelation=patient["autocorrelation"],
+        measurement_error_sd=patient["measurement_error_sd"],
     )
-
-    # TREATMENT1
-    treatment1_array = np.array(
-        list(
-            pd.core.common.flatten(
-                [
-                    [patient["treatment1"] * abs(indicator - 1)]
-                    * treatment_measurements_n
-                    for indicator in patient["treatment_order"]
-                ]
-            )
-        )
-    )
-    # TREATMENT22
-    treatment2_array = np.array(
-        list(
-            pd.core.common.flatten(
-                [
-                    [patient["treatment2"] * indicator] * treatment_measurements_n
-                    for indicator in patient["treatment_order"]
-                ]
-            )
-        )
-    )
-
-    # TREND
-    trend_array = np.array(
-        [
-            patient["trend"] * measurement_index
-            for measurement_index in range(total_measurements_n)
-        ]
-    )
-
-    measurements = ma_process_array + treatment1_array + treatment2_array + trend_array
 
     patient_df = pd.DataFrame(
         {
@@ -299,6 +266,5 @@ patient_measurements_df = pd.concat(patient_measurements_df_list, ignore_index=T
 
 patient_params_df.to_csv(params_path, index=False)
 patient_measurements_df.to_csv(measurements_path, index=False)
-
 
 # %%
---
+++ src/create_patient_measurements.py
@@ -0,0 +1,71 @@
+import os
+
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from dotenv import load_dotenv
+
+load_dotenv()
+seed = int(os.getenv("SEED"))
+
+np.random.seed(seed)
+
+
+def create_patient_measurements(
+    total_measurements_n,
+    treatment_measurements_n,
+    treatment_indicator_list,
+    treatment1,
+    treatment2,
+    measurement_error_sd,
+    autocorrelation=0,
+    trend=None,
+):
+
+    # TREATMENT1
+    treatment1_array = np.array(
+        list(
+            pd.core.common.flatten(
+                [
+                    [treatment1 * abs(indicator - 1)] * treatment_measurements_n
+                    for indicator in treatment_indicator_list
+                ]
+            )
+        )
+    )
+    # TREATMENT2
+    treatment2_array = np.array(
+        list(
+            pd.core.common.flatten(
+                [
+                    [treatment2 * indicator] * treatment_measurements_n
+                    for indicator in treatment_indicator_list
+                ]
+            )
+        )
+    )
+
+    # ERROR TERM
+    # no autoregressive component
+    ar = np.array([1, 0])
+    # lag 1 moving average component
+    ma = np.array([1, autocorrelation])
+    arma_process = sm.tsa.ArmaProcess(ar, ma)
+    error_array = arma_process.generate_sample(
+        nsample=total_measurements_n, scale=measurement_error_sd,
+    )
+
+    measurements = treatment1_array + treatment2_array + error_array
+
+    if trend:
+
+        trend_array = np.array(
+            [
+                trend * measurement_index
+                for measurement_index in range(total_measurements_n)
+            ]
+        )
+
+        measurements = measurements + trend_array
+
+    return measurements
